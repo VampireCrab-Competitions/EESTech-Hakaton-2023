463b11f070c0f01e4a8fbc44049e2c2240581111
TatjanaKnezevic <tanjatutkanknezevic@gmail.com>
2023-03-24 22:22:46 +0100

    Add: Shop scene
    Added Shop scene and connected it to Main Menu scene

--- Assets/Scripts/main_menu.cs
+++ Assets/Scripts/main_menu.cs
@@ -8,6 +8,10 @@ public class main_menu : MonoBehaviour
     public void PlayGame() {
         SceneManager.LoadScene(1);
     }
+    public void OpenShop()
+    {
+        SceneManager.LoadScene(2);
+    }
 
     public void QuitGame() {
         Application.Quit();

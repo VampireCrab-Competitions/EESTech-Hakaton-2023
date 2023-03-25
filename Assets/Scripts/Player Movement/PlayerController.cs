46cc4a50747bd7f41e96ea81758e34a9a4246a7e
mi18145 <tanjatutkanknezevic@gmail.com>
2023-03-25 12:59:19 +0100

    Add: Exit function

--- Assets/Scripts/Player Movement/PlayerController.cs
+++ Assets/Scripts/Player Movement/PlayerController.cs
@@ -1,20 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Tilemaps;
 using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] public static int coins;
+    [SerializeField]
+    public static int coins;
 
-    [SerializeField] public static int health = 10;
+    [SerializeField]
+    public static int health = 10;
 
-    [SerializeField] private int armor;
+    [SerializeField]
+    private int armor;
 
-    [SerializeField] private int mana;
+    [SerializeField]
+    private int mana;
 
-    [SerializeField] private Tilemap floorTilemap;
+    [SerializeField]
+    private Tilemap floorTilemap;
 
-    [SerializeField] private Tilemap colisionTilemap;
+    [SerializeField]
+    private Tilemap colisionTilemap;
 
     [SerializeField]
     private Tilemap treasureTilemap;
@@ -26,7 +38,6 @@ public class PlayerController : MonoBehaviour
     private Tile key_tile;
 
     private PlayerMovement _controls;
-
     private bool hasKey;
 
     private void Awake()
@@ -65,10 +76,18 @@ public class PlayerController : MonoBehaviour
     private void Move(Vector2 direction)
     {
         direction = new Vector2(direction.x * 0.16f, direction.y * 0.16f);
+
         if (CanMove(direction))
         {
             transform.position += (Vector3)direction;
             Vector3Int tile_coords = treasureTilemap.WorldToCell(transform.position);
+
+            Vector3Int exit = treasureTilemap.WorldToCell(new Vector3(terrain_map_generator.exit_x, terrain_map_generator.exit_y,0));
+            if (tile_coords == exit)
+            {
+                SceneManager.LoadScene(0);
+            }
+
             if (treasureTilemap.HasTile(tile_coords))
             {
                 TileBase tile = treasureTilemap.GetTile(tile_coords);
@@ -78,17 +97,20 @@ public class PlayerController : MonoBehaviour
                     GameObject coin_disp = GameObject.Find("Coin Text TMP");
                     coin_disp.GetComponent<TextMeshProUGUI>().text = coins.ToString();
 
-                } else if (String.Equals(tile.name, treasureTiles[1].name))
+                }
+                else if (String.Equals(tile.name, treasureTiles[1].name))
                 {
                     health += 4;
                     GameObject hp_disp = GameObject.Find("HP Text TMP");
                     hp_disp.GetComponent<TextMeshProUGUI>().text = health.ToString();
-                } else if (String.Equals(tile.name, treasureTiles[2].name))
+                }
+                else if (String.Equals(tile.name, treasureTiles[2].name))
                 {
                     coins += 5;
                     GameObject coin_disp = GameObject.Find("Coin Text TMP");
                     coin_disp.GetComponent<TextMeshProUGUI>().text = coins.ToString();
-                } else if (String.Equals(tile.name, key_tile.name))
+                }
+                else if (String.Equals(tile.name, key_tile.name))
                 {
                     GameObject k_disp = GameObject.Find("Hatch Key");
                     //k_disp.GetComponent<Image>().enabled = true;
@@ -98,13 +120,27 @@ public class PlayerController : MonoBehaviour
                 treasureTilemap.SetTile(tile_coords, null);
             }
         }
+        
     }
 
     private bool CanMove(Vector2 direction)
     {
         Vector3Int gridPosition = floorTilemap.WorldToCell(transform.position + (Vector3)direction);
+        bool isExit = gridPosition == floorTilemap.WorldToCell(new Vector3(terrain_map_generator.exit_x, terrain_map_generator.exit_y, 0));
 
-        return floorTilemap.HasTile(gridPosition) && !colisionTilemap.HasTile(gridPosition);
+        return floorTilemap.HasTile(gridPosition) && 
+                ((hasKey && isExit) 
+                || !colisionTilemap.HasTile(gridPosition));
+    }
+
+    public int GetHealth()
+    {
+        return health;
+    }
+
+    public void SetHealth(int health)
+    {
+        health = health;
     }
 
     public int GetMana()
@@ -126,33 +162,9 @@ public class PlayerController : MonoBehaviour
     {
         this.armor = armor;
     }
-<<<<<<< HEAD
-    
+
     public int GetCoins()
     {
         return coins;
     }
 }
-=======
-
-    // public int GetCoins()
-    // {
-    //     return coins;
-    // }
-    //
-    // public void SetCoins(int coins)
-    // {
-    //     this.coins = coins;
-    // }
-
-    // public int GetHealth()
-    // {
-    //     return health;
-    // }
-    //
-    // public void SetHealth(int health)
-    // {
-    //     this.health = health;
-    // }
-}
->>>>>>> f34c587ca652a1ca14a7a0d810e6402d9af54f56

3c6f8cf4478c3d2363bedda950639e69d59c4e54
mi18145 <tanjatutkanknezevic@gmail.com>
2023-03-25 05:07:13 +0100

    Add: Treasure Map
    Added treasures on map

---
+++ Assets/Scripts/Grid Map/treasure_map.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class treasure_map : MonoBehaviour
+{
+    public Tilemap treasure_grid;
+    public Tile[] tiles;
+    public int treasure_chance;
+
+    private int map_size;
+    // Start is called before the first frame update
+    void Start()
+    {
+        map_size = transform.parent.GetComponent<grid_map_fields>().map_size;
+        for (int x = 0; x < map_size; x++)
+        {
+            for (int y = 0; y < map_size; y++)
+            {
+                if (Random.Range(0, 100) <= treasure_chance)
+                {
+                    treasure_grid.SetTile(new Vector3Int(x, y, 0), tiles[Random.Range(0, tiles.Length)]);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
